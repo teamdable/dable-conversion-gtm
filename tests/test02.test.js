f7a59a3fcdc1de85d9747fd595cc880bb8fe2cac
sungcheol <sungcheol@dable.io>
2025-10-14 15:26:10 +0900

    fix timer issue

--- tests/test02.test.js
+++ tests/test02.test.js
@@ -8,43 +8,44 @@ const mockData = {
     debugMode: false
   };
   
-  let dablenaCallArgs = [];
-  
+  let trackedEvents = [];
+
   mock('createArgumentsQueue', function(functionName, queueName) {
-    return function() {
-      dablenaCallArgs.push(arguments);
-    };
+    return function() {};
   });
-  
+
   mock('copyFromWindow', function(name) {
     if (name === '__dablena_gtm_loaded') return true;
+    if (name === '__dablena_script_loaded') return true;
+    if (name === 'dablena') return function() {};
     return undefined;
   });
-  
+
   mock('setInWindow', function() {
     return true;
   });
-  
+
   mock('injectScript', function(url, onSuccess) {
     onSuccess();
   });
-  
+
   mock('makeTableMap', function() {
     return {};
   });
-  
+
+  mock('callInWindow', function(path, eventArray) {
+    trackedEvents.push({
+      event: eventArray[1],
+      params: eventArray[2]
+    });
+    return undefined;
+  });
+
   // 실행
   runCode(mockData);
-  
+
   // 검증
-  assertThat(dablenaCallArgs.length).isGreaterThan(0);
-  // Purchase 이벤트와 파라미터 확인
-  let foundPurchase = false;
-  for (let i = 0; i < dablenaCallArgs.length; i++) {
-    if (dablenaCallArgs[i][1] === 'Purchase') {
-      foundPurchase = true;
-      assertThat(dablenaCallArgs[i][2].value).isEqualTo('15000');
-      assertThat(dablenaCallArgs[i][2].currency).isEqualTo('KRW');
-    }
-  }
-  assertThat(foundPurchase).isTrue();
+  assertThat(trackedEvents.length).isEqualTo(1);
+  assertThat(trackedEvents[0].event).isEqualTo('Purchase');
+  assertThat(trackedEvents[0].params.value).isEqualTo('15000');
+  assertThat(trackedEvents[0].params.currency).isEqualTo('KRW');
--- tests/test03.test.js
+++ tests/test03.test.js
@@ -8,28 +8,35 @@ const mockData = {
   };
   
   let eventTracked = '';
-  
+
   mock('createArgumentsQueue', function(functionName, queueName) {
-    return function(cmd, eventName) {
-      if (cmd === 'track') {
-        eventTracked = eventName;
-      }
-    };
+    return function() {};
   });
-  
+
   mock('copyFromWindow', function(name) {
     if (name === '__dablena_gtm_loaded') return true;
+    if (name === '__dablena_script_loaded') return true;
+    if (name === 'dablena') return function() {};
     return undefined;
   });
-  
+
+  mock('setInWindow', function() {
+    return true;
+  });
+
   mock('injectScript', function(url, onSuccess) {
     onSuccess();
   });
-  
+
   mock('makeTableMap', function() {
     return {};
   });
-  
+
+  mock('callInWindow', function(path, eventArray) {
+    eventTracked = eventArray[1];
+    return undefined;
+  });
+
   runCode(mockData);
-  
+
   assertThat(eventTracked).isEqualTo('event1');
